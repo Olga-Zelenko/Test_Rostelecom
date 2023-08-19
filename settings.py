4c03dff7d7761ba993a4593622258ecbc4febdd8
Olga Zelenko <123513645+Olga-Zelenko@users.noreply.github.com>
2023-08-19 18:10:33 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,24 @@
+class Settings:
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'yavotit275@chodyi.com'
+    valid_password = 'f35r3rfet43FA'
+    valid_email_for_reg = 'pekvyx@mailto.plus' #необходимо менять перед тестом
+    invalid_email = 'ijnsvon@mail.ru'
+    invalid_password = 'gvsuyGZA342'
+    empty_email = ''
+    empty_password = ''
+    menu_of_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name_of_user = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    f_name = 'Иван'
+    l_name = 'Иванов'
+    f_name_ = '-'
+    l_name_ = '-'
+    russian_generate_string = 'л'
+    latin_generate_string = 'qwertyu'
+    chinese_chars = '我不会说汉怎么样你会说英语'
+    special_chars = '!@#${}$*****%^&*(#)_+_+=//\[\}]]]'
+    numbers = 173
+    empty = ''
+    passw1 = 'Qw987654321qw'
+    passw2 = 'Uid125777nklP89683'
+    passw3 = 'IOdd9876543210ELo8964776'
